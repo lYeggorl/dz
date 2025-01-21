820296913a3fb4f324f8011b8fb67c5c8fc0b21d
Yeggor <egorkushnir@bk.ru>
2025-01-21 19:12:24 +0300

    Lesson_14_junit_5

---
+++ src/test/java/org/example/FactorialTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Тест факториала на JUnit")
+
+
+public class FactorialTest {
+    @AfterEach
+    void massagesAfterTests(){
+        System.out.println("Тест закончен");
+    }
+
+    @Tag("Пройдёт")
+    @Test
+    void testFactorialOne()
+    {
+        System.out.println("Test 1");
+        Assertions.assertEquals(120 , Factorial.factorial(5));
+    }
+
+    @Tag("Не Пройдёт")
+    @Test
+    void testFactorialTwo()
+    {
+        System.out.println("Test 2");
+        Assertions.assertEquals(120 , Factorial.factorial(4));
+    }
+}
