fb6c97dba0eee83e1ddaa6681d5f0168f8c0a6ad
Yeggor <egorkushnir@bk.ru>
2025-01-27 05:50:47 +0300

    Исправил

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -1,16 +1,21 @@
 package org.example;
 
 public class Factorial {
-    public static Object factorial(Object number) {
+    public static Object factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException();
+        } else if (number == 0) {
+            return 1;
+        }
         try {
-            int c = (int) number;
-            for (int i = 1; i < (int) number; i++) {
-                c = c * i;
-            }
-            System.out.println("Факториал " + number + " равен " + c);
-            return c;
+            int c = number;
+                for (int i = 1; i < number; i++) {
+                    c = c * i;
+                }
+                System.out.println("Факториал " + number + " равен " + c);
+                return c;
         } catch (Exception e) {
-            return null;
+            throw new RuntimeException(e);
         }
     }
     public static void main(String[] args) {
--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -1,29 +1,37 @@
 package org.example;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
 public class FactorialTest {
     @BeforeSuite
-    public void massageBeforeSuite(){
+    public void massageBeforeSuite() {
         System.out.println("Начало  теста");
     }
     @org.testng.annotations.Test
-     void testFactorialOne()
-    {
+     void testFactorialOne() {
         System.out.println("Test 1");
         assertEquals(Factorial.factorial(2), 2);
     }
-
     @org.testng.annotations.Test
     void testFactorialTwo() {
         System.out.println("Test 2");
         assertEquals(Factorial.factorial(8), 120);
     }
     @org.testng.annotations.Test
-    void testFactorialTree()
-    {
+    void testFactorialThree() {
         System.out.println("Test 3");
-        assertNull(Factorial.factorial(null));
+        Factorial.factorial(0);
+    }
+    @org.testng.annotations.Test
+    void testFactorialFour() {
+        System.out.println("Test 4");
+        assertEquals(Factorial.factorial(1), 1);
+    }
+    @org.testng.annotations.Test(expectedExceptions = IllegalArgumentException.class)
+    void testFactorialFive() {
+        System.out.println("Test 5");
+        Factorial.factorial(-6);
     }
 }
